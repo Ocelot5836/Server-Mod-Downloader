c189761215753a050d60e5f6037b277057a3d4fe
Ocelot <FinntheRaider@gmail.com>
2020-06-02 20:47:35 -0700

    Fixed misnamed button

--- src/main/java/io/github/ocelot/client/screen/DownloadModFilesCompleteScreen.java
+++ src/main/java/io/github/ocelot/client/screen/DownloadModFilesCompleteScreen.java
@@ -23,7 +23,7 @@ public class DownloadModFilesCompleteScreen extends Screen
     @Override
     protected void init()
     {
-        this.addButton(new Button(this.width / 2 - 100, (this.height - 24) / 2, 200, 20, I18n.format("button." + ServerDownloader.MOD_ID + ".restart"), component -> this.getMinecraft().shutdown()));
+        this.addButton(new Button(this.width / 2 - 100, (this.height - 24) / 2, 200, 20, I18n.format("menu.quit"), component -> this.getMinecraft().shutdown()));
         this.addButton(new Button(this.width / 2 - 100, (this.height + 24) / 2, 200, 20, I18n.format("gui.toTitle"), component -> this.getMinecraft().displayGuiScreen(new MainMenuScreen())));
     }
 
